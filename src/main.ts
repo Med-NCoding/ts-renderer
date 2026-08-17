108156b85d30c0a785e2eb3bd5e97df7f538cccf
Medhansh <medhanshnegi4@gmail.com>
2026-08-16 20:18:40 -0400

    add low-poly bunny model

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import {
   vec3Sub, vec3Cross, vec3Normalize, vec3Dot,
 } from './math';
 import { parseObj } from './obj-parser';
-import objText from '../models/suzanne.obj?raw';
+import objText from '../models/bunny.obj?raw';
 import { fillTriangle } from './rasterizer';
 
 const WIDTH  = 640;
